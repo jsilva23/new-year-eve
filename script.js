ce1f5b943c496628b0dddfdace9e13068b88338e
José da Silva <josesilva23p@gmai..com>
2020-12-13 11:37:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const dayEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+const newYear = '1 jan 2021';
+
+function countDown() {
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+    
+    const totalSeconds = (newYearDate - currentDate) / 1000; 
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    dayEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds); 
+
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
