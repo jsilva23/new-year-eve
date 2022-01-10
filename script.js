0c9c2ed30eec1cbe3538d76b76e61288a6a9937c
José da Silva <josesilva23p@gmail.com>
2022-01-10 15:33:39 +0100

    update time

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const minsEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
 
-const newYear = '1 jan 2022';
+const newYear = '1 jan 2023';
 
 function countDown() {
     const newYearDate = new Date(newYear);
